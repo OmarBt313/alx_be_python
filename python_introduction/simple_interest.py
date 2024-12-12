b5f66cec166144382a53ef5cca88102f6a14f1e6
omar <omar.boutoutla@gmail.com>
2024-12-12 23:30:30 +0100

    fixing simple intrest file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,6 +3,6 @@ principal = 1000
 rate = 0.05
 time = 3
 
-simple_intrest = principal * rate * time
+intrest = principal * rate * time
 
-print (f'The simple interest is:{simple_intrest}')
+print (f'The simple interest is:{intrest}')
