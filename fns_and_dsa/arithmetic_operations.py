2d81093a82d0c512febddec96a07ac82c87b73e3
omar <omar.boutoutla@gmail.com>
2025-01-12 20:37:46 +0100

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,7 @@ def perform_operation(num1, num2, operation):
     if operation == 'divide':
         if num1 and num2 != 0:
             return num1 / num2
-        else:
+        elif num2 == 0:
             print('invalid input')
     if operation == 'multiply':
         return num1 * num2
